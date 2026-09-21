8df242c938b259c4d832ab7db3dd5bc55d1e0e83
aryangupta3105 <151863167+aryangupta3105@users.noreply.github.com>
2026-09-22 04:40:58 +0530

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        map<int,int>mpp;
+        for(auto &i:intervals){
+            mpp[i.start]++;
+            mpp[i.end]--;
+
+        }
+        int prev=0,res=0;
+        for(auto &itr:mpp){
+            prev+=itr.second;
+            res=max(res,prev);
+        }
+        return res;
+    }
+};
